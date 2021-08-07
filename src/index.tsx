115f51528e99f19516549f98cf3d3924cd15dd2f
Oğuz Özcan <oguz@tetahq.com>
2021-08-07 13:44:00 +0300

    tsconfig updated. Project configured for React 18

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './app/app';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const rootElement = document.querySelector('#root');
+
+if (!rootElement) {
+  throw new Error('Failed to find the root element');
+}
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/tests/app.test.tsx
+++ src/tests/app.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import App from '../app/app';
+import { render, screen } from '@testing-library/react';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/🖐/i);
   expect(linkElement).toBeInTheDocument();
 });
