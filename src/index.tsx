8802efb9109b0e394727100c3efce68c924a58ed
Oğuz Özcan <oguz@tetahq.com>
2021-08-09 01:47:48 +0300

    (Fixed) Eslint errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/app';
 import reportWebVitals from './reportWebVitals';
@@ -12,9 +12,9 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <StrictMode>
-    <App/>
-  </StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
